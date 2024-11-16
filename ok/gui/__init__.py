02ea9b041304a1d69896dd81999001260f077cbf
firedcto@gmail.com <firedcto@gmail.com>
2024-11-17 00:56:19 +0800

    优化升级

--- ok/gui/__init__.py
+++
@@ -1,6 +0,0 @@
-from ok.OK import OK
-
-ok: OK | None = None
-executor = None
-app = None
-device_manager = None
--- ok/rotypes/Windows/Graphics/__init__.py
+++
@@ -1,5 +0,0 @@
-import ctypes
-
-
-class SizeInt32(ctypes.Structure):
-    _fields_ = [('Width', ctypes.c_int32), ('Height', ctypes.c_int32)]
