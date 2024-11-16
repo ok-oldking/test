446f669e529ebf3b8bc88193309d537be46e22f8
firedcto@gmail.com <firedcto@gmail.com>
2024-11-17 00:31:10 +0800

    优化升级

--- ok/interaction/DoNothingInteraction.py
+++
@@ -1,8 +0,0 @@
-from ok.interaction.BaseInteraction import BaseInteraction
-from ok.logging.Logger import get_logger
-
-logger = get_logger(__name__)
-
-
-class DoNothingInteraction(BaseInteraction):
-    pass
--- ok/util/win.py
+++
@@ -1,9 +0,0 @@
-import ctypes
-
-
-def is_admin():
-    try:
-        # Only Windows users with admin privileges can read the C drive directly
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
